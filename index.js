7538e5c380ef707ce3b42870a98e42944fc6ee8c
AugieRakow <14160783-AugieRakow@users.noreply.replit.com>
2023-09-25 05:55:36 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const axios = require('axios');
+const { App } = require('@slack/bolt');
+const signingSecret = process.env['SLACK_SIGNING_SECRET']
+const botToken = process.env['SLACK_BOT_TOKEN']
+const app = new App({
+  signingSecret: signingSecret,
+  token: botToken,
+});
+
+// Middleware to log errors
+app.error((error) => {
+  console.error('Error:', error);
+});
+
+(async () => {
+  await app.start(process.env.PORT || 12000);
+
+  app.message('quote', async ({ message, say }) => {
+    let resp = await axios.get(`https://api.quotable.io/random`);
+    const quote = resp.data.content;
+    await say(`Hello, <@${message.user}>, ${quote}`);
+  });
+
+
+  console.log(`⚡️ Bolt app is running!`);
+})();
